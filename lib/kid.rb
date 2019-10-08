22b87e8bd9865f69c1071b3e0ee2ec91f9de63b7
Brian Pompey <brianthescholar@gmail.com>
2019-10-08 09:22:11 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,13 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
+  include Dance
+
+  attr_accessor :name
+
+  def initialiaze(name)
+    @name= name
+  end
 
 
 
